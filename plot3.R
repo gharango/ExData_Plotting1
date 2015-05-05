daf12d41f322c7d04104e022efcf134161d8a7f6
gharango <gustavo.arango@oracle.com>
2015-05-05 23:25:55 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,1 +1,13 @@
 
+#get all data after unziping code
+setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
+A=read.csv2("household_power_consumption.txt",header=TRUE,dec=".",colClasses=c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?",NA))
+library("dplyr")
+A=dplyr::filter(A,Date>="2007-02-01" & Date<="2007-02-02")
+png(file="plot3.png")
+with(A, {plot(Time, Sub_metering_1, col="black",type="l", ylab= "Energy sub metering", xlab="")
+         lines(Time, Sub_metering_2, col="red")
+		 lines(Time, Sub_metering_3, col="blue")
+		 })
+legend("topright", col=c("black","red","blue"), lty=1,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
