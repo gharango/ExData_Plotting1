a9df68e6d9c4016acd5e6510f719af85b054c54f
gharango <gharango@users.noreply.github.com>
2015-05-06 23:30:15 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,11 +3,13 @@
 setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
 A=read.csv2(unz("household_power_consumption.zip","household_power_consumption.txt"),header=TRUE,dec=".",colClasses=c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?",NA))
 
+#subsetting
 library("dplyr")
 A=dplyr::filter(A,Date>="2007-02-01" & Date<="2007-02-02")
 
 #setting time values
 A$Time = strptime(paste(as.character(A$Date),A$Time),"%Y-%m-%d %H:%M:%S")
+
 #plotting
 png(file="plot2.png")
 with(A, plot(Time, Global_active_power, type="l", ylab= "Global Active Power (kilowatts)", xlab=""))
