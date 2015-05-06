768429136ed13e789b5a2b9336629abf1161eb4a
gharango <gharango@users.noreply.github.com>
2015-05-06 23:23:38 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,9 +1,13 @@
 
-#get all data after unziping code
+#get all data from zipped file
 setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
 A=read.csv2(unz("household_power_consumption.zip","household_power_consumption.txt"),header=TRUE,dec=".",colClasses=c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?",NA))
 library("dplyr")
 A=dplyr::filter(A,Date>="2007-02-01" & Date<="2007-02-02")
+
+#setting time values
+A$Time = strptime(paste(as.character(A$Date),A$Time),"%Y-%m-%d %H:%M:%S")
+
 png(file="plot3.png")
 with(A, {plot(Time, Sub_metering_1, col="black",type="l", ylab= "Energy sub metering", xlab="")
          lines(Time, Sub_metering_2, col="red")
