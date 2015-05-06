50857a1e9692334ee05aa3f74b0067b177b3b0d0
gharango <gharango@users.noreply.github.com>
2015-05-06 23:22:29 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -4,6 +4,9 @@ setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
 A=read.csv2(unz("household_power_consumption.zip","household_power_consumption.txt"),header=TRUE,dec=".",colClasses=c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?",NA))
 library("dplyr")
 A=dplyr::filter(A,Date>="2007-02-01" & Date<="2007-02-02")
+#setting time values
+A$Time = strptime(paste(as.character(A$Date),A$Time),"%Y-%m-%d %H:%M:%S")
+
 #plotting
 png(file="plot4.png")
 par(mfrow = c(2, 2))
