b9b6a3fe51f36e1f907162e574a13b98e64ee520
gharango <gharango@users.noreply.github.com>
2015-05-06 23:11:52 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 
 #get all data after unziping code
 setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
-A=read.csv2("household_power_consumption.txt",header=TRUE,dec=".",colClasses=c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?",NA))
+A=read.csv2(unz("household_power_consumption.zip","household_power_consumption.txt"),header=TRUE,dec=".",colClasses=c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?",NA))
 library("dplyr")
 A=dplyr::filter(A,Date>="2007-02-01" & Date<="2007-02-02")
 png(file="plot3.png")
